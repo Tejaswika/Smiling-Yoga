d342dbf200af2acbe085d55619ca67b56e38e9e0
Tejaswika <teju050301@gmail.com>
2020-07-21 21:47:59 +0530

    Updates

---
+++ precache-manifest.24a2f33431e80bf68cc51683a849e123.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63e79e83b0be7934f433a617771b828",
+    "url": "/Smiling-Yoga/index.html"
+  },
+  {
+    "revision": "0534f36150776455c315",
+    "url": "/Smiling-Yoga/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "55d0904c92db750cf56a",
+    "url": "/Smiling-Yoga/static/css/main.52fecec5.chunk.css"
+  },
+  {
+    "revision": "0534f36150776455c315",
+    "url": "/Smiling-Yoga/static/js/2.3acb9c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Smiling-Yoga/static/js/2.3acb9c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d0904c92db750cf56a",
+    "url": "/Smiling-Yoga/static/js/main.90b8fce0.chunk.js"
+  },
+  {
+    "revision": "7957288e573aed7221f0",
+    "url": "/Smiling-Yoga/static/js/runtime-main.7d18cd23.js"
+  },
+  {
+    "revision": "5bcc67ede678fe4016b200fac21c2acd",
+    "url": "/Smiling-Yoga/static/media/11111-100.5bcc67ed.jpg"
+  },
+  {
+    "revision": "d0712891f639a785289605150d626060",
+    "url": "/Smiling-Yoga/static/media/34-100.d0712891.jpg"
+  },
+  {
+    "revision": "bfdcc3ce5c373f6c23b84cf79bfad33b",
+    "url": "/Smiling-Yoga/static/media/342-100.bfdcc3ce.jpg"
+  },
+  {
+    "revision": "d5461a2c3441ef7583595d5057919b5e",
+    "url": "/Smiling-Yoga/static/media/345-100.d5461a2c.jpg"
+  },
+  {
+    "revision": "2714503fba18bf0a646968b308f8bde0",
+    "url": "/Smiling-Yoga/static/media/3455-100.2714503f.jpg"
+  },
+  {
+    "revision": "41babe2f8d080691f553bc8d400d6e07",
+    "url": "/Smiling-Yoga/static/media/4567-100.41babe2f.jpg"
+  },
+  {
+    "revision": "f66a6077cc6eee3c5f2d2b475f90064d",
+    "url": "/Smiling-Yoga/static/media/666666-100.f66a6077.jpg"
+  },
+  {
+    "revision": "e58a4ea6fd97b448ff4dbb2914652e93",
+    "url": "/Smiling-Yoga/static/media/678-100.e58a4ea6.jpg"
+  },
+  {
+    "revision": "e804c89803091c010b139bb8b423d7a7",
+    "url": "/Smiling-Yoga/static/media/6789-100.e804c898.jpg"
+  },
+  {
+    "revision": "adb30d9367794e9065e3acc973312e3b",
+    "url": "/Smiling-Yoga/static/media/7899-100.adb30d93.jpg"
+  },
+  {
+    "revision": "b1d87a15706bfe04d287ae213f392282",
+    "url": "/Smiling-Yoga/static/media/9087-100.b1d87a15.jpg"
+  },
+  {
+    "revision": "2ee349900911b4b06aef74210a87a055",
+    "url": "/Smiling-Yoga/static/media/Artboard 90-100.2ee34990.jpg"
+  },
+  {
+    "revision": "71b4ce3528c12f75d01e42787d4fe86d",
+    "url": "/Smiling-Yoga/static/media/Flat_People_doing_Yoga_01_by_MissChatz-100.71b4ce35.jpg"
+  },
+  {
+    "revision": "2083e927714ff1745cf822732ae2bfe9",
+    "url": "/Smiling-Yoga/static/media/Mesa de trabajo 1 copia-100.2083e927.jpg"
+  }
+]);
