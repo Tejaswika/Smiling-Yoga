f7be814ef14b4d8a8d3e928c89aca37dcbdce342
Tejaswika <65081403+Tejaswika@users.noreply.github.com>
2020-07-23 17:49:55 +0530

    update componets

---
+++ components/BlockTwo.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class BlockTwo extends Component {
+  render() {
+    return (
+      <div className="blockTwo">
+        <div className="music">
+          <img
+            className="musicIcon"
+            src={require("./Images/Artboard 90-100.jpg")}
+          />
+          <p className="song">SONG</p>
+          <p className="songArtist">SONG ARTIST</p>
+        </div>
+        <div className="music">
+          <img
+            className="musicIcon"
+            src={require("./Images/Artboard 90-100.jpg")}
+          />
+          <p className="song">SONG</p>
+          <p className="songArtist">SONG ARTIST</p>
+        </div>
+        <div className="music">
+          <img
+            className="musicIcon"
+            src={require("./Images/Artboard 90-100.jpg")}
+          />
+          <p className="song">SONG</p>
+          <p className="songArtist">SONG ARTIST</p>
+        </div>
+        <div className="music" id="Artisthidden">
+          <img
+            className="musicIcon"
+            src={require("./Images/Artboard 90-100.jpg")}
+          />
+          <p className="song">SONG</p>
+          <p className="songArtist">SONG ARTIST</p>
+        </div>
+
+        <div className="button2" style={{ bottom: "-9vh" }}>
+          VIEW MORE
+        </div>
+      </div>
+    );
+  }
+}
+export default BlockTwo;
